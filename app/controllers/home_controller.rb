106ead41c26f142b9451629ec5e88794042ca738
yunseobshin <skwi2004@gmail.com>
2016-04-11 14:33:48 +0000

    brick

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
     def index
         
     end
-    
-    def wrtie
-        
+    def write
+        @aaa = params[:title]
+        @bbb = params[:content]
     end
 end
